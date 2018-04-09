9980a8044da2360141af453c60cfb3596f92ca84
Marc Stutz <marc.stutz@footlocker.com>
2018-04-09 13:44:11 +0200

    for the sake of progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
+
+  doThing = () => {
+    this.props.selectRecipe({})
+  }
 
   render() {
     console.log('Props', this.props)
@@ -13,7 +18,7 @@ class App extends Component {
   }
 }
 
-function mapStateToProps (calendar) {
+function mapStateToProps ({ calendar, food }) {
   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
   return {
@@ -21,7 +26,7 @@ function mapStateToProps (calendar) {
       day,
       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
         meals[meal] = calendar[day][meal]
-          ? calendar[day][meal]
+          ? food[calendar[day][meal]]
           : null
 
         return meals
@@ -30,4 +35,11 @@ function mapStateToProps (calendar) {
   }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
